1be21599708d20846464afa4eae1653f50cb5ce8
Lucas Alvine <lucasalvine@lucasalvine-Dell-System-Vostro-3450>
2018-07-17 15:05:57 -0500

    incomplete koans

---
+++ about_hashes.rb
@@ -0,0 +1,63 @@
+  def test_hash_keys
+    hash = { :one => "uno", :two => "dos" }
+    assert_equal 2, hash.keys.size
+    assert_equal true, hash.keys.include?(:one)
+    assert_equal true, hash.keys.include?(:two)
+    assert_equal Array, hash.keys.class
+  end
+
+  def test_hash_values
+    hash = { :one => "uno", :two => "dos" }
+    assert_equal 2, hash.values.size
+    assert_equal true, hash.values.include?("uno")
+    assert_equal true, hash.values.include?("dos")
+    assert_equal Array, hash.values.class
+  end
+
+  def test_combining_hashes
+    hash = { "jim" => 53, "amy" => 20, "dan" => 23 }
+    new_hash = hash.merge({ "jim" => 54, "jenny" => 26 })
+
+    assert_equal true, hash != new_hash
+
+    expected = { "jim" => 54, "amy" => 20, "dan" => 23, "jenny" => 26}
+    assert_equal true, expected == new_hash
+  end
+
+  def test_default_value
+    hash1 = Hash.new
+    hash1[:one] = 1
+
+    assert_equal 1, hash1[:one]
+    assert_equal nil, hash1[:two]
+
+    hash2 = Hash.new("dos")
+    hash2[:one] = 1
+
+    assert_equal 1, hash2[:one]
+    assert_equal nil, hash2[:two]
+  end
+
+  def test_default_value_is_the_same_object
+    hash = Hash.new([])
+
+    hash[:one] << "uno"
+    hash[:two] << "dos"
+
+    assert_equal "uno", hash[:one]
+    assert_equal "dos", hash[:two]
+    assert_equal nil, hash[:three]
+
+    assert_equal Array, hash[:one].object_id == hash[:two].object_id
+  end
+
+  def test_default_value_with_block
+    hash = Hash.new {|hash, key| hash[key] = [] }
+
+    hash[:one] << "uno"
+    hash[:two] << "dos"
+
+    assert_equal "uno", hash[:one]
+    assert_equal "dos", hash[:two]
+    assert_equal nil, hash[:three]
+  end
